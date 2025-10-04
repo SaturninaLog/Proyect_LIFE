f4023327cf25c7b039556b006d0fbff8871b1313
SaturninaLog <ruizacuna6@gmail.com>
2025-10-04 00:21:20 -0500

    VersionUI
    Aqui esta la ui simple para reemplazar

---
+++ Proyect LIFE/Assets/Scripts/Botones/InteraccionBotones.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InteraccionBotones : MonoBehaviour
+{
+    // --- PARA CARGAR ESCENAS ---
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    // --- PARA ABRIR Y CERRAR PANELES ---
+
+    //Activa el panel que se coloque en el inspector
+    public void OpenPanel(GameObject panel)
+    {
+        panel.SetActive(true);
+    }
+
+    //desactiva el panel que se coloque en el inspector
+    public void ClosePanel(GameObject panel)
+    {
+        panel.SetActive(false);
+    }
+
+    // Alterna el estado del panel (si está activo lo cierra, si no lo abre)
+    public void TogglePanel(GameObject panel)
+    {
+        panel.SetActive(!panel.activeSelf);
+    }
+}
